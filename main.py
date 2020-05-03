87228b4a5ea02fffef26ed3f4dde1f8e9869bbb5
GildasPhilippe <gildas.philippe56@gmail.com>
2020-05-04 00:07:56 +0200

    Loaded the boxes from the json and created the object

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
 # This is an attempt to recreate the game Monopoly, because we do not want to pay
 # for it on Steam, and mostly because we are very bored during this quarantine.
 
-import numpy as np
-import random as rd
 import json
-import src.classes
+from src.classes import User, Board, Street, Box, Game
+
 
 def initGame():
-    streets = []
-    jsonDict = json.loads("boxes.json")
+
+    with open("boxes.json") as boxes_file:
+        json_boxes = json.loads(boxes_file.read())
+
+    boxes = []
+
+    for box in json_boxes:
+        if json_boxes[box]["type"] == "street":
+            boxes.append(
+                Street(
+                    int(box), 
+                    json_boxes[box]["type"], 
+                    json_boxes[box]["name"], 
+                    json_boxes[box]["price"], 
+                    json_boxes[box]["rent"], 
+                    json_boxes[box]["color"]
+                )
+            )
+        else:
+            boxes.append(
+                Box(
+                    int(box),
+                    json_boxes[box]["type"],
+                    json_boxes[box]["name"]
+                )
+            )
+    return Board(boxes)
+
+
+if __name__ == "__main__":
+    board = initGame()
+    for box in board.boxes:
+        print("{} - TYPE : {} \t\t NAME : {}".format(box.index, box.box_type, box.name))
+
 
 
 
--- src/classes.py
+++ src/classes.py
@@ -1,44 +1,48 @@
 # Those are the classes to represent the game
 
-import numpy as np
-import random as rd
-
 INITIAL_MONEY = 1500
 NB_CASES = 40
 
+
 class User:
-    "A simple class to describe the lambda monopoly player"
-    def __init__(self, name, money = INITIAL_MONEY, position = 0, goods = []):
+    """A simple class to describe the lambda monopoly player"""
+    def __init__(self, name, money=INITIAL_MONEY, position=0, goods=[]):
         self.name = name
         self.money = money
         self.position = position
         self.goods = goods
 
+
 class Box:
-     "A simple class to describe each box"
-     def __init__(self, name, players):
-         self.name = name
-         self.players = players    #which player is on this box
-         self.type = type
+    """A simple class to describe each box"""
+
+    def __init__(self, index, box_type, name, players=[]):
+        self.index = index
+        self.box_type = box_type
+        self.name = name
+        self.players = players  # which players are on this box
+
 
 class Street(Box):
-    "A simple class to describe a good on the board, like LES GALERIES LAFAYETTE"
-    def __init__(self, name, price, rent, players = [], owner = None, color = 0):
-        super().__init__(name, players)
+    """"A simple class to describe a good on the board, like LES GALERIES LAFAYETTE"""
+    def __init__(self, index,  box_type, name, price, rent, players=[], owner=None, color=0):
+        super().__init__(index,  box_type, name, players)
         self.owner = owner
         self.price = price
         self.rent = rent
         self.color = color
 
+
 class Board:
-    "A simple class to describe the board"
+    """A simple class to describe the board"""
     def __init__(self, boxes):
         self.boxes = boxes
 
+
 class Game:
-    "A simple class to describe the game globally"
+    """"A simple class to describe the game globally"""
     def __init__(self, players, board, turn, dice):
-        this.players = players
-        this.board = board
-        this.turn = turn
-        this.dice = dice
+        self.players = players
+        self.board = board
+        self.turn = turn
+        self.dice = dice
