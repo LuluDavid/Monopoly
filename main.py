e0b016eb27a869894c1ae14fd3538a290a3f7549
Chloe Mouret <chloe.mouret62@orange.fr>
2020-05-04 18:08:13 +0200

    first version

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 # for it on Steam, and mostly because we are very bored during this quarantine.
 
 import json
+import random
 from src.classes import User, Board, Street, Box, Game
+
 
 
 def initGame():
@@ -33,14 +35,100 @@ def initGame():
                 )
             )
     return Board(boxes)
+    
+def initPlayers():    #beginning of the game only
+    playersNumber = input("Combien y a-t-il de joueurs ?")
+    while (int(playersNumber)<=1):
+        playersNumber = input("Le jeu doit comporter au minimum 2 joueurs. Combien y a-t-il de joueurs ? ")
+    L=[]     #list with all the names of the players
+    for i in range(int(playersNumber)):
+        player = input("Quel est le nom du joueur"+str(i+1)+" ?")
+        L.append(User(player))
+    return L
+    
+def orderOfPlayers(L):
+    input("Bienvenue à tous dans cette super partie de Monopoly, nous avons tout d'abord tirer au sort l'ordre de jeu des différents joueurs")
+    random.shuffle(listOfPlayers)
+    n=len(listOfPlayers)
+    for i in range(n):
+        print("Le joueur "+str(i+1)+" est "+listOfPlayers[i].name)
+    return listOfPlayers
+    
+    
+def actualizePosition(order, i, dice):
+    oldPosition = order[i].position
+    if (oldPosition+dice < 40):
+        order[i].position = oldPosition + dice
+    else :
+        order[i].position = (oldPosition+dice-40)
+        
+        
+def buyAStreetOrNot(player):
+    pos = player.position
+    case=board.boxes[pos]
+    if (case.box_type == "street"):
+        price = case.price
+        if (case.owner == None  and player.money>=price):
+            choice = input("Cette propriété est libre, son prix est de "+str(price)+" euros. Voulez-vous l'acheter ? (Il vous reste "+str(player.money)+ " euros)")
+            if (choice == "oui"):
+                player.goods.append(board.boxes[pos])
+                board.boxes[pos].owner = player
+                player.money = player.money - price
+                input("Vous venez d'acheter la propriété "+ str(case.name) +". Il vous reste "+str(player.money)+ " euros.")
+            else : 
+                input("Vous avez décider de ne pas acheter, vous avez toujours "+str(player.money)+" euros.")
+        else : 
+            if (case.owner == None and player.money<price):
+                input("Cette propriété est libre, malheureusement vous n'avez pas assez d'argent pour l'acheter. Il vous reste "+ str(player.money) +" euros et le prix est de "+str(price) + " euros.")
+            else :      #the street belongs to someone
+                owner = case.owner.name
+                if (case.owner == player):
+                    input("Cette propriété vous appartient. Vous ne pouvez rien faire")
+                else:
+                    rent = case.rent
+                    print("Cette propriété appartient à "+owner+", vous lui devez "+str(rent)+" euros.")
+                    player.money = player.money - rent
+                    print("Il vous reste "+str(player.money)+" euros.")
+                    case.owner.money = case.owner.money + rent
+                    input(""+owner+" gagne "+str(rent)+" euros, il lui reste "+str(case.owner.money)+" euros.")
+    else:
+        input("ce n'est pas une rue, pour l'instant vous ne pouvez pas l'acheter.")
+                
+    
+    
+def turn(order):                         #one turn
+    numberOfPlayers = len(order)
+    for i in range(numberOfPlayers):
+        dice=random.randint(1,12)
+        print(""+order[i].name+", tu as fait "+ str(dice))
+        actualizePosition(order,i,dice)
+        pos = order[i].position
+        playerStreetPosition = board.boxes[pos].name
+        input("Tu es sur la case : " + playerStreetPosition)
+        buyAStreetOrNot(order[i])
+    for player in order:
+        if (player.money <0):
+            input(""+ player.name+ ", tu as perdu! Tu n'as plus d'argent.")
+            order.remove(player)
+    return order
+    
+    
+    
 
 
 if __name__ == "__main__":
     board = initGame()
-    for box in board.boxes:
-        print("{} - TYPE : {} \t\t NAME : {}".format(box.index, box.box_type, box.name))
-
-
+    #for box in board.boxes:
+        #print("{} - TYPE : {} \t\t NAME : {}".format(box.index, box.box_type, box.name))
+    listOfPlayers = initPlayers()
+    numberOfPlayers = len(listOfPlayers)
+    order = orderOfPlayers(listOfPlayers)
+    while len(order)>=2:
+        order = turn(order)
+    print(""+order[0].name+"!! TU AS GAGNE !!!!!!")
+    
+        
+    
 
 
 
