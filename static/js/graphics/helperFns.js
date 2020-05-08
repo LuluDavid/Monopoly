364d6c5d50a45a888f60c0617c3f4d50c29b83d0
Lucien David <davidlulu92@yahoo.fr>
2020-05-08 18:04:21 +0200

    Adding houses to boxes

--- static/js/graphics/helperFns.js
+++ static/js/graphics/helperFns.js
@@ -13,10 +13,22 @@ function init() {
 	numberOfPawns = 6
 	pawnHeight = 2
 	pawnRadius = 0.5
+	// House Ratio
+	houseRatio = 1/7
 	// Box
 	coverRatio = 0.7
 	boxWidth = cardboardWidth/12.2
 	boxHeight = 1.6*boxWidth
+	boxHeightHouse = boxHeight*(1-houseRatio)
+	boxHeightHouseBand = boxHeight*houseRatio
+	maxNumberOfHouse = 4
+	// House band width
+	houseWidthBox = boxHeight*houseRatio
+	// House
+	houseWidth = 1
+	houseHeight = houseWidth/2
+	roofAngle = Math.PI/4
+	roofSize = houseWidth/(2*Math.cos(roofAngle))
 	// Box positions
 	boxes = getBoxesPositions(cardboardWidth)
 	// Accuracy for pawn motion
@@ -25,7 +37,7 @@ function init() {
 	// Whose turns it is
 	currentPawn = 0
 	// Pawn motion
-	tMotion = 3 // duration to go to next case (seconds)
+	tMotion = 0.5 // duration to go to next case (seconds)
 	// Pawn positions on boxes
 	boxes = getBoxesPositions(cardboardWidth);
 	// Pawn positions per box (where to put them to make them fit in)
@@ -37,6 +49,11 @@ function init() {
 	closeViewRatio = 0.4
 	closeViewHeight = 50
 	closeViewFurtherRatio = 2
+	// House relative position
+	houseRelativePos = getHouseRelativePositions()
+	housePositions = getHousesPositions(cardboardWidth)
+	// Number of house per box
+	numberOfHousesPerBox = noHousesPerBox()
 
 
 	/*
@@ -85,8 +102,10 @@ function init() {
 	cardboard.castShadow = true;
 	cardboard.receiveShadow = true;
 	// Create and add the pawns
-	let pawns = createPawns(numberOfPawns);
-	addPawns(pawns, scene);
+	let pawnObjects = createPawns(numberOfPawns);
+	pawns = new THREE.Group()
+	addPawns(pawnObjects, pawns);
+	scene.add(pawns);
 	// Set the positions of the pawns on the cardboard in positions (fast access to positions)
 	positions = initPositions();
 	new_positions = initPositions();
@@ -94,7 +113,16 @@ function init() {
 	/*
 	 * First test to move successfully pawns around the board
 	 */
-	$(document).on('click',() => incrementPositionsAux());
+	// $(document).on('click',() => incrementPositionsAux());
+
+	
+	/*
+	 * Create a group of empty houses per box
+	 */
+	housesPerBox = updateAllHouses()
+
+	i = 12
+	$(document).on('click',() => incrementHousesPerBox(i));
 
 	/*
 	 * Basic update functions : update OrbiteControls, update the camera
@@ -103,8 +131,6 @@ function init() {
 	animate();
 
 }
-
-
 
 
 
@@ -204,7 +230,7 @@ function emptyPositions(){
 function updateNewPositions(){
 	new_positions = emptyPositions();
 	for (let i = 0; i<numberOfPawns; i++){
-		let box = scene.children[3+i].currentBox;
+		let box = scene.children[3].children[i].currentBox;
 		new_positions[box].push(i);
 	}
 }
@@ -240,7 +266,7 @@ function incrementPositions(){
 	var dices = randomDices();
 	console.log("Vous obtenez un score de "+dices)
 	// Copies
-	var pawn = Object.assign(scene.children[3+currentPawn])
+	var pawn = Object.assign(scene.children[3].children[currentPawn])
 	var currentBox = Object.assign(pawn.currentBox)
 	// Update the box where the pawn is (modulo numberOfBoxes) 
 	currentBox += dices;
@@ -261,7 +287,7 @@ function translatePawnToBox(i, j){
 	let boxCardinal = Math.max(positions[j].length, new_positions[j].length)
 	let pawnPosition = pawnsPositionsPerBox[j][boxCardinal]
 	let deltaT = tMotion*Math.pow(j/numberOfBoxes, 1/3)
-	scene.children[3+i].currentBox = j;
+	scene.children[3].children[i].currentBox = j;
 	updateNewPositions();
 	addCanvas();
 	translate(i, new THREE.Vector3(pawnPosition.x, pawnPosition.y, pawnHeight/2 + 0.05), deltaT)
@@ -290,7 +316,7 @@ function translate(i, goalPosition, deltaT){
 	var fps = 60;           // seconds
 	var step = 1 / (deltaT * fps);  // t-step per frame
 	var t = 0;
-	object = scene.children[3+i];
+	object = scene.children[3].children[i];
 	var initialPosition = object.position.clone()
 	var initialCameraPosition = closeView.camera.position.clone()
 	var goalPositionCamera = new THREE.Vector3(goalPosition.x, goalPosition.y, goalPosition.z + closeViewHeight)
@@ -343,8 +369,52 @@ function loop2(step, t) {
  */
 
 
+function getHousePositions(i){
+	/*
+	
+	---------------------------------------
+	| <-> ||| <-> ||| <-> ||| <-> ||| <-> |
+	---------------------------------------
+	
+	*/
+	let housePositions = []
+	let box = boxes[i]
+	if (i % 10 == 0){
+		console.log("On peut pas mettre de maisons sur les coins connard.")
+		return;
+	}
+	let width, height
+	if (box.isW){
+		width = boxHeightHouseBand;
+		height = boxWidth;
+	}
+	if (box.isH){
+		width = boxWidth;
+		height = boxHeightHouse;
+	}
 
-// TODO: externalize the common part (the first lines) for the three different cases (corner, width, height)
+
+	
+
+	return houseDiffPos;
+}
+
+function getHouseRelativePositions(){
+
+	let space = (boxWidth - maxNumberOfHouse*houseWidth)/(maxNumberOfHouse+1)
+	let offset = space-boxWidth/2
+	let housePos = []
+
+	for (let i = 0; i<maxNumberOfHouse; i++){
+		let newPos = offset + houseWidth/2
+		housePos.push(newPos)
+		offset += houseWidth + space
+	}
+
+	return housePos
+}
+
+
 function getPawnPositions(i){
 	/*
 	_____________________________
@@ -362,10 +432,21 @@ function getPawnPositions(i){
 	*/
 	// Get the box parameters
 	let box = boxes[i]
-	let width = boxWidth;
-	let height = boxWidth;
-	if (box.isW) width = boxHeight;
-	if (box.isH) height = boxHeight;
+	let width, height
+	if (box.isW){
+		if (box.isH){
+			width = boxHeight;
+			height = boxHeight;
+		}
+		else{
+			width = boxHeightHouse;
+			height = boxWidth;
+		}
+	}
+	else if (box.isH){
+			width = boxWidth;
+			height = boxHeightHouse;
+	}
 
 	// Just take the ratio-ed width and height to spread the pawns
 	widthRatio = coverRatio*width
@@ -405,6 +486,58 @@ function getPawnPositions(i){
 	return positions
 }
 
+/*
+ * Update houses per box
+ */
+
+function incrementHousesPerBox(i){
+	numberOfHousesPerBox[i]++
+	updateAllHouses()
+}
+
+function updateAllHouses(){
+	scene.children[4] = updateHouseGroup()
+}
+
+function noHousesPerBox(){
+	let nbHousesPerBox = {}
+	for (let i = 0; i<numberOfBoxes; i++){
+		nbHousesPerBox[i] = 0
+	}
+	return nbHousesPerBox
+}
+
+function updateHouseGroup(){
+	var housesPerBox = new THREE.Group()
+	for (let i = 0; i<numberOfBoxes; i++){
+		let houses = updateHouses(i)
+		housesPerBox.add(houses)
+	}
+	return housesPerBox
+}
+
+function updateHouses(i){
+	let houses = new THREE.Group()
+
+	for (let j = 0; j<numberOfHousesPerBox[i]; j++){
+		let house = setUpHouse(i,j)
+		houses.add(house)
+	}
+	return houses
+}
+
+function setUpHouse(i,j){
+	let box = boxes[i]
+	let house = createHouse()
+	if (box.isH){
+		house.position.set(housePositions[i].x, housePositions[i].y+houseRelativePos[j], 0.05)
+	}
+	else if (box.isW){
+		house.position.set(housePositions[i].x+houseRelativePos[j], housePositions[i].y, 0.05)
+	}
+	return house
+}
+
 
 
 
@@ -441,6 +574,74 @@ function createPawn(position){
 	return pawn;
 }
 
+function createHouse(){
+
+	var house = new THREE.Group();
+	let wallGeometry = new THREE.PlaneGeometry(houseWidth, houseHeight, 300)
+	let roofGeometry = new THREE.PlaneGeometry(houseWidth, roofSize)
+	let roofFrontGeometry = new THREE.Geometry();
+	roofFrontGeometry.vertices.push(new THREE.Vector3(-houseWidth/2,houseWidth/2,houseHeight));
+	roofFrontGeometry.vertices.push(new THREE.Vector3(-houseWidth/2,-houseWidth/2,houseHeight));
+	roofFrontGeometry.vertices.push(new THREE.Vector3(0,0,houseHeight+houseWidth*Math.tan(roofAngle)/2))
+	roofFrontGeometry.faces.push( new THREE.Face3( 0, 1, 2 ) );
+	roofFrontGeometry.faces.push( new THREE.Face3( 0, 2, 1 ) );
+	let roofBackGeometry = new THREE.Geometry();
+	roofBackGeometry.vertices.push(new THREE.Vector3(houseWidth/2,houseWidth/2,houseHeight));
+	roofBackGeometry.vertices.push(new THREE.Vector3(houseWidth/2,-houseWidth/2,houseHeight));
+	roofBackGeometry.vertices.push(new THREE.Vector3(0,0,houseHeight+houseWidth*Math.tan(roofAngle)/2))
+	roofFrontGeometry.faces.push( new THREE.Face3( 0, 1, 2 ) );
+	roofBackGeometry.faces.push( new THREE.Face3( 0, 2, 1 ) );
+
+	var material = new THREE.MeshBasicMaterial( {color:0xff0000, side:THREE.DoubleSide} )
+	var materialSides = new THREE.MeshBasicMaterial( {color:0xff0000, side:THREE.DoubleSide} )
+	let frontWall = new THREE.Mesh(wallGeometry, material)
+	let backWall = new THREE.Mesh(wallGeometry, material)
+	let leftWall = new THREE.Mesh(wallGeometry, material)
+	let rightWall = new THREE.Mesh(wallGeometry, material)
+	let leftRoof = new THREE.Mesh(roofGeometry, material)
+	let rightRoof = new THREE.Mesh(roofGeometry, material)
+	let roofFront = new THREE.Mesh(roofFrontGeometry, materialSides)
+	let roofBack = new THREE.Mesh(roofBackGeometry, materialSides)
+
+	frontWall.castShadow = true;
+	frontWall.receiveShadow = true;
+	backWall.castShadow = true;
+	backWall.receiveShadow = true;
+	leftWall.castShadow = true;
+	leftWall.receiveShadow = true;
+	rightWall.castShadow = true;
+	rightWall.receiveShadow = true;
+	leftRoof.castShadow = true;
+	leftRoof.receiveShadow = true;
+	rightRoof.castShadow = true;
+	rightRoof.receiveShadow = true;
+
+	frontWall.position.set(-houseWidth/2, 0, houseHeight/2)
+	backWall.position.set(houseWidth/2, 0, houseHeight/2)
+	leftWall.position.set(0, houseWidth/2, houseHeight/2)
+	rightWall.position.set(0, -houseWidth/2, houseHeight/2)
+	leftRoof.position.set(0, houseWidth/4, houseHeight+houseWidth/4*Math.tan(roofAngle))
+	rightRoof.position.set(0, -houseWidth/4, houseHeight+houseWidth/4*Math.tan(roofAngle))
+	frontWall.rotateX(Math.PI/2)
+	frontWall.rotateY(Math.PI/2)
+	backWall.rotateX(Math.PI/2)
+	backWall.rotateY(Math.PI/2)
+	leftWall.rotateX(Math.PI/2)
+	rightWall.rotateX(Math.PI/2)
+	leftRoof.rotateX(-roofAngle)
+	rightRoof.rotateX(roofAngle)
+	house.add(frontWall)
+	house.add(backWall)
+	house.add(leftWall)
+	house.add(rightWall)
+	house.add(leftRoof)
+	house.add(rightRoof)
+	house.add(roofFront)
+	house.add(roofBack)
+
+	return house;
+}
+
 function createCardboard(width, parentNode){
 	let cardboardGeometry = new THREE.PlaneGeometry(width,width);
 	let texture = new THREE.TextureLoader().load('textures/monopoly.jpg');
@@ -465,8 +666,8 @@ function createGround(parentNode){
 }
 
 function addALight(parentNode){
-	let light = new THREE.DirectionalLight(0xffffff,1);
-	light.position.set(0,10,10)
+	let light = new THREE.DirectionalLight(0xffffff,1.2);
+	light.position.set(200,200,200)
 	light.castShadow = true;
 	light.shadow.mapSize.width = 2048;
 	light.shadow.mapSize.height = 2048;
@@ -502,12 +703,12 @@ function getRandomColor() {
 	}
 
 
-function getBoxesPositions(L) {
+function getHousesPositions(L) {
     var l = L / 12.2
     var h = l * 1.6
 
     var axCoords = {
-      0: h/2,
+      0: h-houseWidthBox,
       1: h+l/2,
       2: h+3*l/2,
       3: h+5*l/2,
@@ -517,11 +718,73 @@ function getBoxesPositions(L) {
       7: h+13*l/2,
       8: h+15*l/2,
       9: h+17*l/2,
-      10: L-h/2
+      10: L-h+houseWidthBox
     }
+    var positions = {
+        0: {},
+        1: {x: axCoords[0], y: axCoords[1]},
+        2: {x: axCoords[0], y: axCoords[2]},
+        3: {x: axCoords[0], y: axCoords[3]},
+        4: {x: axCoords[0], y: axCoords[4]},
+        5: {x: axCoords[0], y: axCoords[5]},
+        6: {x: axCoords[0], y: axCoords[6]},
+        7: {x: axCoords[0], y: axCoords[7]},
+        8: {x: axCoords[0], y: axCoords[8]},
+        9: {x: axCoords[0], y: axCoords[9]},
+        10: {},
+        11: {x: axCoords[1], y: axCoords[10]},
+        12: {x: axCoords[2], y: axCoords[10]},
+        13: {x: axCoords[3], y: axCoords[10]},
+        14: {x: axCoords[4], y: axCoords[10]},
+        15: {x: axCoords[5], y: axCoords[10]},
+        16: {x: axCoords[6], y: axCoords[10]},
+        17: {x: axCoords[7], y: axCoords[10]},
+        18: {x: axCoords[8], y: axCoords[10]},
+        19: {x: axCoords[9], y: axCoords[10]},
+        20: {},
+        21: {x: axCoords[10], y: axCoords[9]},
+        22: {x: axCoords[10], y: axCoords[8]},
+        23: {x: axCoords[10], y: axCoords[7]},
+        24: {x: axCoords[10], y: axCoords[6]},
+        25: {x: axCoords[10], y: axCoords[5]},
+        26: {x: axCoords[10], y: axCoords[4]},
+        27: {x: axCoords[10], y: axCoords[3]},
+        28: {x: axCoords[10], y: axCoords[2]},
+        29: {x: axCoords[10], y: axCoords[1]},
+        30: {},
+        31: {x: axCoords[9], y: axCoords[0]},
+        32: {x: axCoords[8], y: axCoords[0]},
+        33: {x: axCoords[7], y: axCoords[0]},
+        34: {x: axCoords[6], y: axCoords[0]},
+        35: {x: axCoords[5], y: axCoords[0]},
+        36: {x: axCoords[4], y: axCoords[0]},
+        37: {x: axCoords[3], y: axCoords[0]},
+        38: {x: axCoords[2], y: axCoords[0]},
+        39: {x: axCoords[1], y: axCoords[0]}
+    };
+  return positions;
+}
 
-    positions = {
-        0: {x: axCoords[0], y: axCoords[0], isW:true, isH:true},
+
+function getBoxesPositions(L) {
+    var l = L / 12.2
+    var h = l * 1.6
+
+    var axCoords = {
+      0: h/2-houseWidthBox,
+      1: h+l/2,
+      2: h+3*l/2,
+      3: h+5*l/2,
+      4: h+7*l/2,
+      5: h+9*l/2,
+      6: h+11*l/2,
+      7: h+13*l/2,
+      8: h+15*l/2,
+      9: h+17*l/2,
+      10: L-h/2+houseWidthBox
+    }
+    var positions = {
+        0: {x: h/2, y: h/2, isW:true, isH:true},
         1: {x: axCoords[0], y: axCoords[1], isW:false, isH:true},
         2: {x: axCoords[0], y: axCoords[2], isW:false, isH:true},
         3: {x: axCoords[0], y: axCoords[3], isW:false, isH:true},
@@ -531,7 +794,7 @@ function getBoxesPositions(L) {
         7: {x: axCoords[0], y: axCoords[7], isW:false, isH:true},
         8: {x: axCoords[0], y: axCoords[8], isW:false, isH:true},
         9: {x: axCoords[0], y: axCoords[9], isW:false, isH:true},
-        10: {x: axCoords[0], y: axCoords[10], isW:true, isH:true},
+        10: {x: h/2, y: L-h/2, isW:true, isH:true},
         11: {x: axCoords[1], y: axCoords[10], isW:true, isH:false},
         12: {x: axCoords[2], y: axCoords[10], isW:true, isH:false},
         13: {x: axCoords[3], y: axCoords[10], isW:true, isH:false},
@@ -541,7 +804,7 @@ function getBoxesPositions(L) {
         17: {x: axCoords[7], y: axCoords[10], isW:true, isH:false},
         18: {x: axCoords[8], y: axCoords[10], isW:true, isH:false},
         19: {x: axCoords[9], y: axCoords[10], isW:true, isH:false},
-        20: {x: axCoords[10], y: axCoords[10], isW:true, isH:true},
+        20: {x: L-h/2, y: L-h/2, isW:true, isH:true},
         21: {x: axCoords[10], y: axCoords[9], isW:false, isH:true},
         22: {x: axCoords[10], y: axCoords[8], isW:false, isH:true},
         23: {x: axCoords[10], y: axCoords[7], isW:false, isH:true},
@@ -551,7 +814,7 @@ function getBoxesPositions(L) {
         27: {x: axCoords[10], y: axCoords[3], isW:false, isH:true},
         28: {x: axCoords[10], y: axCoords[2], isW:false, isH:true},
         29: {x: axCoords[10], y: axCoords[1], isW:false, isH:true},
-        30: {x: axCoords[10], y: axCoords[0], isW:true, isH:true},
+        30: {x: L-h/2, y: h/2, isW:true, isH:true},
         31: {x: axCoords[9], y: axCoords[0], isW:true, isH:false},
         32: {x: axCoords[8], y: axCoords[0], isW:true, isH:false},
         33: {x: axCoords[7], y: axCoords[0], isW:true, isH:false},
