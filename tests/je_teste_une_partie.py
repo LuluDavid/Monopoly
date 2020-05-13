94725813335fd2f12a993bb35fab657c69c88c8d
Chloe Mouret <chloe.mouret62@orange.fr>
2020-05-13 20:29:33 +0200

    modif pour faire fonctionner avec les listes des joueurs sur les cases. Rq : tests a completer

--- tests/je_teste_une_partie.py
+++ tests/je_teste_une_partie.py
@@ -5,7 +5,7 @@ from game.board import Board
 
 if __name__ == '__main__':
     #players = {0: "Chloe", 1: "Lucien", 2: "Gildas", 3: "Camille"}
-    game = Game({0: "Chloe", 1: "Lucien", 2: "Gildas"})
+    game = Game({0: "Chloe", 1: "Samy"})
     order = game.orderOfPlayers(game.players)
     while len(order) > 1:
         game.turn()
